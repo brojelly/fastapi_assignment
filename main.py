fcee20dd043dda4d0ae2d3b740efaed8d7b36a7e
muk <lhm4802@nate.com>
2025-05-24 12:37:50 +0900

    day1 중급과제

---
+++ main.py
@@ -0,0 +1,61 @@
+# main.py
+
+from typing import Annotated
+
+from fastapi import FastAPI, Path, HTTPException, Query
+
+from app.models.users import UserModel
+from app.schemas.users import UserCreatRequest, UserUpdateRequest, UserSearchParams
+
+app = FastAPI()
+
+@app.post("/users")
+async def create_user(data: UserCreatRequest):
+    user = UserModel.create(**data.dict())
+    return user.id
+
+@app.get("/users")
+async def get_all_users():
+    result = UserModel.all()
+    if not result:
+        raise HTTPException(status_code=404, detail="User not found")
+    return result
+
+@app.get("/users/{user_id}")
+async def get_user(user_id:int = Path(gt = 0)):
+    user = UserModel.get(id = user_id)
+    if not user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@app.patch("/users/{user_id}")
+async def update_user(data: UserUpdateRequest, user_id:int = Path(gt = 0)):
+    user = UserModel.get(id = user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    user.update(**data.model_dump())
+    return user
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id:int = Path(gt = 0)):
+    user = UserModel.get(id = user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    user.delete()
+    return {"detail": f"User:{user_id}, deleted"}
+
+@app.get('/users/search')
+async def search_users(query_params: Annotated[UserSearchParams, Query()]):
+    valid_query = {key: value for key, value in query_params.model_dump().items() if value is not None}
+    filtered_users = UserModel.filter(**valid_query)
+    if not filtered_users:
+        raise HTTPException(status_code=404)
+    return filtered_users
+
+
+UserModel.create_dummy()  # API 테스트를 위한 더미를 생성하는 메서드 입니다.
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host='0.0.0.0', port=8000)
