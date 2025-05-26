3ea733cd8509d5fa98bf277a44c2575a17c265d2
muk <lhm4802@nate.com>
2025-05-26 21:40:54 +0900

    day2

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from typing import Annotated
 
 from fastapi import FastAPI, Path, HTTPException, Query
 
+from app.models.movies import MovieModel
 from app.models.users import UserModel
+from app.schemas.movies import MovieResponse, CreateMovieRequest, MovieSearchParams, MovieUpdateRequest
 from app.schemas.users import UserCreatRequest, UserUpdateRequest, UserSearchParams
 
 app = FastAPI()
@@ -24,7 +26,7 @@ async def get_all_users():
 @app.get("/users/{user_id}")
 async def get_user(user_id:int = Path(gt = 0)):
     user = UserModel.get(id = user_id)
-    if not user is None:
+    if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
@@ -53,7 +55,47 @@ async def search_users(query_params: Annotated[UserSearchParams, Query()]):
     return filtered_users
 
 
-UserModel.create_dummy()  # API 테스트를 위한 더미를 생성하는 메서드 입니다.
+@app.get('/movies', response_model=list[MovieResponse], status_code=200)
+async def get_movies(query_params: Annotated[MovieSearchParams, Query()]):
+    valid_query = {key: value for key, value in query_params.model_dump().items() if value is not None}
+
+    if valid_query:
+        return MovieModel.filter(**valid_query)
+
+    return MovieModel.all()
+
+@app.post('/movies', response_model=MovieResponse, status_code=201)
+async def create_movie(data: CreateMovieRequest):
+	movie = MovieModel.create(**data.model_dump())
+	return movie
+
+@app.get('/movies/{movie_id}', response_model=MovieResponse, status_code=200)
+async def get_movie(movie_id: int = Path(gt=0)):
+	movie = MovieModel.get(id=movie_id)
+	if movie is None:
+		raise HTTPException(status_code=404)
+	return movie
+
+@app.patch('/movies/{movie_id}', response_model=MovieResponse, status_code=200)
+async def edit_movie(data: MovieUpdateRequest, movie_id: int = Path(gt=0)):
+	movie = MovieModel.get(id=movie_id)
+	if movie is None:
+		raise HTTPException(status_code=404)
+	movie.update(**data.model_dump())
+	return movie
+
+@app.delete('/movies/{movie_id}', status_code=204)
+async def delete_movie(movie_id: int = Path(gt=0)):
+	movie = MovieModel.get(id=movie_id)
+	if movie is None:
+		raise HTTPException(status_code=404)
+	movie.delete()
+	return
+
+
+UserModel.create_dummy()
+MovieModel.create_dummy()
+# API 테스트를 위한 더미를 생성하는 메서드 입니다.
 
 if __name__ == '__main__':
     import uvicorn
